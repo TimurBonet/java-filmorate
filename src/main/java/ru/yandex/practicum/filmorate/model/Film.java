bae6c942bbde33e8a8b8d0ebcdfdba6eaad9c0e8
TimurBonet <lightsword@yandex.ru>
2024-01-20 23:14:05 +0700

    ФП10 исправление стиля кода 5

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import lombok.ToString;
-import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
